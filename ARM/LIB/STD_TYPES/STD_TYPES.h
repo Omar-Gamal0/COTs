0e9fd7b49dbb2b45f3e9efdc529b9612d346b0c8
Omar Gamal <omar.gamal.eldin0@gmail.com>
2020-09-08 04:05:27 +0200

    Added f32 to STD_TYPES.h

--- ARM/LIB/STD_TYPES/STD_TYPES.h
+++ ARM/LIB/STD_TYPES/STD_TYPES.h
@@ -1,7 +1,7 @@
 /***********************************************/
 /* Author	: Omar Gamal						/ 
-/* Date		: 25 AUG 2020						/
-/* Version 	: V02								/
+/* Date		: 08 Sept 2020						/
+/* Version 	: V03								/
 /***********************************************/
 #ifndef STD_TYPES_H
 #define STD_TYPES_H
@@ -9,6 +9,8 @@
 typedef unsigned char 				u8;
 typedef unsigned short int 			u16;
 typedef unsigned long int 			u32;
+
+typedef float f32;
 
 #define __V							volatile
 
